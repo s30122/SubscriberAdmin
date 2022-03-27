530b236b677a5cd7936490fc04c71382b791f096
嬋娟 <s30122@hotmail.com>
2022-03-27 17:05:59 +0800

    add client proj

---
+++ SubscriberClient/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SubscriberClient.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
